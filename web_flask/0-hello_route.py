92570a8b00f5b35ea77285406441ce2c6acf811a
Okagua Kenoye <kenoyek@gmail.com>
2023-04-22 09:57:54 +0100

    All Taaks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
@@ -9,7 +10,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
